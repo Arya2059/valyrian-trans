91d1b1582cb905ad3f1cf4b97dd58678edc8d724
Arya <119836600+Arya2059@users.noreply.github.com>
2022-12-06 15:34:51 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Speak Yodish
+
+## About
+
+This is a fun translation app where the user can translate English language to Yodish Language!
+
+## Tech used
+
+- **Languages used:** HTML, CSS and JavaScript
---
+++ index.js
@@ -0,0 +1,21 @@
+var btnTranslate = document.querySelector(".btn-translate");
+var txtInput = document.querySelector("#txt-input");
+var output = document.querySelector(".output");
+
+function clickEventHandler() {
+  translate(txtInput.value);
+}
+
+btnTranslate.addEventListener("click", clickEventHandler);
+
+function translate(inputText) {
+  fetch(
+    `https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json?text=${inputText}`
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (json) {
+      output.innerHTML = json.contents.translated;
+    });
+}
